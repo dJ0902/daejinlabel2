4d509ddc0b96194cf0f13dc0cce76f8c8e8067a5
Leejoongjae123 <ljj3347@naver.com>
2024-09-03 13:48:44 +0900

    first commit

---
+++ app/compoenents/Navbar.js
@@ -0,0 +1,76 @@
+"use client";
+import Script from "next/script";
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  Link,
+  Button,
+  Divider,
+} from "@nextui-org/react";
+import { Icon } from "@iconify/react";
+
+import { AcmeIcon } from "./acme";
+import { cn } from "./cn";
+
+const menuItems = ["Home"];
+
+export default function Nav(props) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  return (
+    <Navbar
+      {...props}
+      classNames={{
+        base: cn("border-default-100", {
+          "bg-default-200/50 ": isMenuOpen,
+        }),
+        wrapper: "w-full justify-center",
+        item: "hidden md:flex",
+      }}
+      height="60px"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+    >
+      {/* Left Content */}
+      <Link href="/">
+        <NavbarBrand>
+          <span className="ml-2 text-2xl font-bold">커스텀 라벨</span>
+        </NavbarBrand>
+      </Link>
+
+      
+
+
+      <NavbarMenu
+        className="top-[calc(var(--navbar-height)_-_1px)] max-h-fit bg-default-200/50 pb-6 pt-6 shadow-medium backdrop-blur-md backdrop-saturate-150 dark:bg-default-100/50"
+        motionProps={{
+          initial: { opacity: 0, y: -20 },
+          animate: { opacity: 1, y: 0 },
+          exit: { opacity: 0, y: -20 },
+          transition: {
+            ease: "easeInOut",
+            duration: 0.2,
+          },
+        }}
+      >
+
+
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link className="mb-2 w-full text-default-500" href="#" size="md">
+              {item}
+            </Link>
+            {index < menuItems.length - 1 && <Divider className="opacity-50" />}
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+
+    </Navbar>
+  );
+}
