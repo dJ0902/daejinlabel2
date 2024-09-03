0c7fb60841e4853c8e7d921f32640fb6d5216136
Leejoongjae123 <ljj3347@naver.com>
2024-09-03 15:52:14 +0900

    240903

--- app/compoenents/Footer.js
+++ app/compoenents/Footer.js
@@ -2,10 +2,12 @@ import React from "react";
 import Link from "next/link";
 function Footer() {
   return (
-    <footer class=" rounded-lg dark:bg-gray-900 m-4 w-screen md:w-1/2">
+    <footer class=" rounded-lg m-4 w-screen md:w-1/2">
       <div class="w-full mx-auto p-4 md:py-8">
         <div class="flex flex-col justify-center items-center">
-          <img src="/images/qr.png" alt="logo" class=" w-[60vw] md:w-[20vw]  h-[60vw] md:h-[20vw]" />
+          <a href="/images/qr.png" download>
+            <img src="/images/qr.png" alt="logo" class=" w-[60vw] md:w-[20vw]  h-[60vw] md:h-[20vw]" />
+          </a>
           <div>
             <span class="text-black font-bold text-2xl">
               QR코드
