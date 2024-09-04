f5c1a43ef392bf41272540219ea249d6f9251707
Leejoongjae123 <ljj3347@naver.com>
2024-09-04 21:17:04 +0900

    240904

---
+++ app/test/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ImageOverlay from '@/app/components/ImageOverlay'
+function page() {
+  return (
+    <div>
+      <ImageOverlay />
+    </div>
+  )
+}
+
+export default page
